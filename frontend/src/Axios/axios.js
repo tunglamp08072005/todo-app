ac747c0b2cbfe7a757e32d287f88b9e4bbe755f0
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-11 16:55:32 +0700

    axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,23 @@
-import axios from "axios"
+import axios from 'axios';
+
+// Tạo một instance của axios với cấu hình sẵn
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
-})
-export default instance
+  baseURL: 'http://localhost:5000', // URL của backend (thay đổi nếu cần)
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Optional: Tự động thêm token vào headers (nếu có)
+instance.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem('authToken'));
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
