efc24f3a724e275ac635df34960deef34c27abc1
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-11 17:32:12 +0700

    userReducer.js

--- frontend/src/reducer/userReducer.js
+++ frontend/src/reducer/userReducer.js
@@ -1,13 +1,16 @@
 function userReducer(user, action) {
-    console.log("userreducer");
-    switch(action.type){
-        case "SET_USER":{
-            return action.payload;
-        }
-        case "UNSET_USER":{
-            return {};
-        }
-        default: return user;
-    }
+  console.log("userReducer");
+
+  switch (action.type) {
+    case "SET_USER":
+      return action.payload;
+
+    case "UNSET_USER":
+      return {};
+
+    default:
+      return user;
+  }
 }
+
 export default userReducer;
