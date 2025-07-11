96d1cd4a4136a99b78fdf53f335f03505d0e5aba
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-11 17:33:59 +0700

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,87 @@
 import './App.css';
 import { useEffect, useReducer } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import AllTask from './components/AllTask';
 import Active from './components/Active';
 import Completed from './components/Completed';
-import AllTask from './components/AllTask';
 import Layout from './components/Layout';
-import TaskContext from './context/TaskContext';
-import TokenContext from './context/TokenContext';
-import taskReducer from './reducer/taskReducer';
-import tokenReducer from './reducer/tokenReducer';
-import userReducer from './reducer/userReducer';
-import Header from './components/Header/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 import ForgotPassword from './components/forgotPassword/ForgotPassword';
 import ResetPassword from './components/forgotPassword/ResetPassword';
-import axios from './Axios/axios.js';
+
+import axios from './Axios/axios';
+
+import TaskContext from './context/TaskContext';
+import TokenContext from './context/TokenContext';
+
+import taskReducer from './reducer/taskReducer';
+import tokenReducer from './reducer/tokenReducer';
+import userReducer from './reducer/userReducer';
+
 function App() {
-  const token = JSON.parse(localStorage.getItem("authToken"));
-  const [tasks, dispatch] = useReducer(taskReducer, [])
-  const [userToken, tokenDispatch] = useReducer(tokenReducer, token)
-  const [user, userDispatch] = useReducer(userReducer, {})
+  const token = JSON.parse(localStorage.getItem('authToken'));
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+  const [userToken, tokenDispatch] = useReducer(tokenReducer, token);
+  const [user, userDispatch] = useReducer(userReducer, {});
+
+  // ✅ Lấy thông tin người dùng
   useEffect(() => {
-    console.log("App.js");
     const fetchUser = async () => {
       try {
-        console.log("fetchUser");
-        const res = await axios.get("/user/getUser",{
+        const res = await axios.get('/user/getUser', {
           headers: {
-            Authorization: `Bearer ${userToken}`
-          }
-        })
-        //tokenDispatch({type: "SET_TOKEN", payload: res.token})
-        console.log("res.data: ", res.data);
-        userDispatch({type: "SET_USER", payload:res.data.user})
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+        userDispatch({ type: 'SET_USER', payload: res.data.user });
       } catch (error) {
-        console.log(error);
+        console.error('Fetch User Error:', error);
+        tokenDispatch({ type: 'UNSET_TOKEN' });
       }
-    }
+    };
+
     if (userToken) {
-      fetchUser()
+      fetchUser();
     }
-  },[userToken])
+  }, [userToken]);
+
+  // ✅ Lấy danh sách công việc
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        console.log("fetchTasks");
-        const res = await axios.get("/task/getTask", {
+        const res = await axios.get('/task/getTask', {
           headers: {
-            Authorization: `Bearer ${userToken}`
-          }
-        })
-        dispatch({ type: "SET_TASK", payload: res.data })
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+        dispatch({ type: 'SET_TASK', payload: res.data });
       } catch (error) {
-        console.log(error);
+        console.error('Fetch Tasks Error:', error);
       }
-    }
+    };
+
     if (userToken) {
-      fetchTasks()
+      fetchTasks();
     }
-  },[userToken])
+  }, [userToken]);
+
   return (
     <BrowserRouter>
-      <TokenContext.Provider value={{userToken, tokenDispatch, user, userDispatch}}>
+      <TokenContext.Provider value={{ userToken, tokenDispatch, user, userDispatch }}>
         <TaskContext.Provider value={{ tasks, dispatch }}>
           <Routes>
             <Route path="/" element={<Header />}>
-              <Route path='/' element={token ? <Layout /> : <Login />}>
-                <Route index element={<AllTask />} />
-                <Route path="active" element={<Active />} />
-                <Route path="completed" element={<Completed />} />
-              </Route>
+              <Route
+                index
+                element={userToken ? <Layout /> : <Login />}
+              />
+              <Route path="/active" element={userToken ? <Active /> : <Login />} />
+              <Route path="/completed" element={userToken ? <Completed /> : <Login />} />
+
+              {/* Auth */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/forgotPassword" element={<ForgotPassword />} />
@@ -79,7 +90,6 @@ function App() {
           </Routes>
         </TaskContext.Provider>
       </TokenContext.Provider>
-
     </BrowserRouter>
   );
 }
